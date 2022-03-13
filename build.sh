c07601eaf15fe064318cbef7458ffa0ce036bf05
Salim Haniff <shaniff@parabellyx.com>
2022-03-13 10:48:29 -0400

    changed git URL in build.sh

--- build.sh
+++ build.sh
@@ -10,9 +10,8 @@ echo -n "vm.max_map_count=524288" | sudo tee -a /etc/sysctl.conf
 
 # Clone repo
 mkdir -p /opt/iris && cd /opt/iris
-git clone https://github.com/parabellyx/iris-infra.git
-cd iris-infra
-git checkout release/v0.2
+git clone https://github.com/parabellyx/iris-infra-dair.git
+cd iris-infra-dair
 
 # Change the password based on env vars
 RANDOM_STR=`openssl rand -base64 21`
@@ -22,5 +21,6 @@ sed -i "s/##password##/$RANDOM_STR/g" secrets/pgsql.secret
 
 kubectl kustomize | kubectl apply -f -
 
+#Should at a wait here for the jenkins container to come into a running status
 echo "sudo kubectl exec -it --namespace=appsec $(sudo kubectl get pods --namespace=appsec | awk '/jenkins/ {print $1}') -- cat /var/jenkins_home/secrets/initialAdminPassword" > /opt/iris/fetchJenkinsCred.sh
 chmod +x /opt/iris/fetchJenkinsCred.sh
