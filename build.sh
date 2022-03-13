9454349d3306390555a4c2f197ff7d131043a0c9
Salim Haniff <shaniff@parabellyx.com>
2022-03-13 10:44:13 -0400

    first commit

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install k3s
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.23.4+k3s1 sh -
+
+
+# Configure Linux
+sysctl -w vm.max_map_count=524288
+echo -n "vm.max_map_count=524288" | sudo tee -a /etc/sysctl.conf
+
+# Clone repo
+mkdir -p /opt/iris && cd /opt/iris
+git clone https://github.com/parabellyx/iris-infra.git
+cd iris-infra
+git checkout release/v0.2
+
+# Change the password based on env vars
+RANDOM_STR=`openssl rand -base64 21`
+sed -i "s/##password##/$RANDOM_STR/g" secrets/pgsql.secret
+sed -i "s/##password##/$RANDOM_STR/g" secrets/pgsql.secret
+
+
+kubectl kustomize | kubectl apply -f -
+
+echo "sudo kubectl exec -it --namespace=appsec $(sudo kubectl get pods --namespace=appsec | awk '/jenkins/ {print $1}') -- cat /var/jenkins_home/secrets/initialAdminPassword" > /opt/iris/fetchJenkinsCred.sh
+chmod +x /opt/iris/fetchJenkinsCred.sh
